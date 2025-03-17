daf49fe26ada3b642dbe748f8c494abfafa9ecd8
Abdurrahman Hakim Makarim <102575410+Furohito@users.noreply.github.com>
2025-03-18 00:06:00 +0700

    yes

---
+++ projek/latihan/src/main/java/com/example/Testing.java
@@ -0,0 +1,10 @@
+package com.example;
+
+import java.util.logging.Logger;
+
+public class Testing {
+    final static Logger log = Logger.getLogger(Testing.class.getName());
+    public static void main(String[] args) {
+        log.info("Hello World");
+    }
+}
